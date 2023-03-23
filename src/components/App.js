3cac74975feb82620ecb8505e13218a0f5b85e6d
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-23 11:53:21 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const jsonData = await response.json();
+      setData(jsonData);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data fetched from API:</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
